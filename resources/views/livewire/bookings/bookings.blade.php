fdb1e65ac97dd819f83efc9458749ab56d44b306
atikahas <45572226+atikahas@users.noreply.github.com>
2023-08-28 12:10:06 +0800

    update

--- resources/views/livewire/bookings/bookings.blade.php
+++ resources/views/livewire/bookings/bookings.blade.php
@@ -1,15 +1,15 @@
 <div>
     <div class="flex justify-end">
-      <div class="text-sm breadcrumbs">
-          <ul>
-              <li><a href="{{ route('dashboard') }}">Home</a></li> 
-              <li>Booking</li> 
-          </ul>
-      </div>
+        <div class="text-sm breadcrumbs">
+                <ul>
+                    <li><a href="{{ route('dashboard') }}">Home</a></li> 
+                    <li>Booking</li> 
+                </ul>
+        </div>
     </div>
     <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight mb-4">
-          Booking
+            Booking
         </h2>
         <div class="flex flex-col w-full sm:flex-row gap-4">
             <div class="sm:w-1/5">
@@ -20,14 +20,6 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
                             List
-                        </a>
-                    </li>
-                    <li>
-                        <a wire:click="create()">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                            Create New
                         </a>
                     </li>
                     <li>
@@ -44,6 +36,14 @@
                 @endif
                 <div class="card w-full bg-base-100 shadow">
                     <div class="card-body">
+                        <div class="card-actions justify-end">
+                        <a wire:click="create()" class="btn btn-sm btn-neutral">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            New Booking
+                        </a>
+                        </div>
                         <div class="overflow-x-auto">
                             <table class="table table-sm table-fixed w-full">
                                 <thead>
